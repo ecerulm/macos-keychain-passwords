c5a04417e2c9c0da30b3b987e57f31b050a20060
Ruben Laguna <ruben.laguna@gmail.com>
2023-04-03 23:22:08 +0200

    Implemented set_password

--- keychain.cc
+++ keychain.cc
@@ -1,28 +1,73 @@
 // hello.cc
 #include <node_api.h>
+#include <Security/Security.h>
 
-namespace demo {
+namespace osxkeychainservices {
 
-napi_value Method(napi_env env, napi_callback_info args) {
-  napi_value greeting;
-  napi_status status;
 
-  status = napi_create_string_utf8(env, "world from Node-API", NAPI_AUTO_LENGTH,
-                                   &greeting);
-  if (status != napi_ok)
-    return nullptr;
-  return greeting;
+napi_value set_password(napi_env env, napi_callback_info args) {
+    // get the arguments from napi_callback_info
+    size_t argc =3;
+    napi_value argv[3];
+    napi_get_cb_info(env, args, &argc, argv, nullptr, nullptr);
+
+    // get the first rgument as a string
+    size_t str_len;
+    napi_get_value_string_utf8(env, argv[0], NULL, 0, &str_len);
+    char* service = new char[str_len + 1];
+    napi_get_value_string_utf8(env, argv[0], service, str_len + 1, &str_len);
+    CFStringRef cfstrService = CFStringCreateWithCString(NULL, service, kCFStringEncodingUTF8);
+    delete [] server;
+
+    // get the second argument as a CFStringRef
+    napi_get_value_string_utf8(env, argv[1], NULL, 0, &str_len);
+    char* account = new char[str_len + 1];
+    napi_get_value_string_utf8(env, argv[1], account, str_len + 1, &str_len);
+    CFStringRef cfstrAccount = CFStringCreateWithCString(NULL, account, kCFStringEncodingUTF8);
+    delete [] account;
+
+    // get the second argument as a CFStringRef
+    napi_get_value_string_utf8(env, argv[2], NULL, 0, &str_len);
+    char* password = new char[str_len + 1];
+    napi_get_value_string_utf8(env, argv[2], password, str_len + 1, &str_len);
+    CFStringRef cfstrPassword= CFStringCreateWithCString(NULL, password, kCFStringEncodingUTF8);
+    delete [] password;
+
+    CFStringRef keys[] = { kSecClass,                 kSecAttrAccount,     kSecValueData,      kSecAttrService };
+    CFTypeRef values[] = { kSecClassGenericPassword,  cfstrAccount,        cfstrPassword,      cfstrService};
+
+    CFDictionaryRef query = CFDictionaryCreate(
+        NULL,
+        (const void **) keys,
+        values,
+        sizeof(keys) / sizeof(keys[0]),
+        &kCFTypeDictionaryKeyCallBacks,
+        &kCFTypeDictionaryValueCallBacks
+    );
+    OSStatus err = SecItemAdd(query, NULL);
+
+    //free cfstrPassword
+    CFRelease(cfstrPassword);
+    CFRelease(cfstrAccount);
+    CFRelease(cfstrService);
+    CFRelease(query);
+
+    if (err != errSecSuccess) {
+        napi_throw_error(env, "ERR", "Error adding item to keychain");
+    }
+
+    return nullptr;  
 }
 
 napi_value init(napi_env env, napi_value exports) {
   napi_status status;
   napi_value fn;
 
-  status = napi_create_function(env, nullptr, 0, Method, nullptr, &fn);
+  status = napi_create_function(env, nullptr, 0, set_password, nullptr, &fn);
   if (status != napi_ok)
     return nullptr;
 
-  status = napi_set_named_property(env, exports, "hello4", fn);
+  status = napi_set_named_property(env, exports, "set_password", fn);
   if (status != napi_ok)
     return nullptr;
   return exports;
