696e462f45262151e071b59b442215ff07a4e782
Ruben Laguna <ruben.laguna@gmail.com>
2023-04-05 15:21:37 +0200

    Add eslint to pre-commit hooks

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+// SPDX-FileCopyrightText: 2023 Ruben Laguna <ruben.laguna@gmail.com>
+//
+// SPDX-License-Identifier: MIT
+
+module.exports = {
+  "env" : {"browser" : true, "commonjs" : true, "es2021" : true},
+  "extends" : "eslint:recommended",
+  "overrides" : [],
+  "parserOptions" : {"ecmaVersion" : "latest"},
+  "rules" : {}
+}
